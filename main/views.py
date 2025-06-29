51277aed7712b3aefd3841fe92e34ca866dd0ee4
Aleksander Lukashov <lukashov784@gmail.com>
2025-06-29 23:23:31 +0700

    twenty_third: caching added

--- main/views.py
+++ main/views.py
@@ -2,24 +2,27 @@ from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
+from common.mixins import CacheMixin
 from main.models import Page
 
 
-class IndexView(TemplateView):
+class IndexView(CacheMixin, TemplateView):
     template_name = 'main/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = _('Home page')
-        context['content'] = get_object_or_404(Page, key='main').content
+        query = Page.objects.get(key='main')
+        context['content'] = self.find_cache(query, 'content_main', 60 * 60)
         return context
 
 
-class AboutView(TemplateView):
+class AboutView(CacheMixin, TemplateView):
     template_name = 'main/about.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = _('About page')
-        context['content'] = get_object_or_404(Page, key='about').content
+        query = Page.objects.get(key='about')
+        context['content'] = self.find_cache(query, 'content_about', 60*60)
         return context
